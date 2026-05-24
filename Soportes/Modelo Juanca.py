75eded4af1a5f8b530c68bf92c1721dca6e8a2c7
j.hernandezj09 <j.hernandezj@uniandes.edu.co>
2026-05-24 01:57:30 -0500

    se importaron librerias necesarias para empezar el modelo de regresion

---
+++ Soportes/Modelo Juanca.py
@@ -0,0 +1,13 @@
+
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.callbacks import EarlyStopping
